88a61dce2aba436e830cfcb675ac1de79315ce09
Janko Zorbas <zorbasjanko@gmail.com>
2025-04-14 10:11:14 +0200

    Added voice line triggers and invisible colliders now don't have bullet hole decals on them

---
+++ Assets/VoicelineTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class VoicelineTrigger : MonoBehaviour
+{
+    [SerializeField] private string voiceLineName;
+    [SerializeField] private bool triggerOnce = true;
+
+    private bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (triggerOnce && hasTriggered) return;
+
+        hasTriggered = true;
+
+        AudioManager.Instance.PlaySound(voiceLineName);
+    }
+}
